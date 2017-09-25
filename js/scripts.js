02c7a300958a5e0dc28f44a7fa514d9505900364
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-09-25 12:47:25 +0200

    pierwotna wersja

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+var a = 8,
+	h = 10,
+	triangleArea = a*h/2;
+
+alert('wynik = ' + triangleArea + ' jednostek^2');
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
